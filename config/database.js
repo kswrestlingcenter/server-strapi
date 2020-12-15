c85b3a56e56720847f594f8584de445eb8450624
Ambra <ambra.isenberg@gmail.com>
2020-12-15 14:58:43 -0600

    update

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      "connector": "mongoose",
-      "settings": {
-        "uri": process.env.MONGO_CLIENT_KWC
+      connector: "mongoose",
+      settings: {
+        uri: process.env.MONGO_CLIENT_KWC
       },
-      "options": {
-        "ssl": true
+      options: {
+        ssl: true
       }
     }
   },
